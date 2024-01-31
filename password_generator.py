d9e6821bfe18565b1651ccc932f256c97dac7676
hdshashank <hdshashankgowda@gmail.com>
2024-02-01 00:09:07 +0530

    Added Password generator

--- password_generator.py
+++ password_generator.py
@@ -11,10 +11,18 @@ def genpass(n):
         print("Password should have atleast 6 characters")
 
 
-lower=list(string.ascii_lowercase)
-upper=list(string.ascii_uppercase)
-digits=list(string.digits)
-symbols=list(string.punctuation)
-pass_list=lower+upper+digits+symbols
-n=int(input("Enter length(>6) of the password:"))
-genpass(n)
+while True:
+    lower=list(string.ascii_lowercase)
+    upper=list(string.ascii_uppercase)
+    digits=list(string.digits)
+    symbols=list(string.punctuation)
+    pass_list=lower+upper+digits+symbols
+    n=int(input("Enter length(>6) of the password:"))
+    genpass(n)
+    cont=input("Do you want to generate one more password(y/n):")
+    if cont.lower()=='n':
+        break
+    elif cont.lower()=='y':
+        continue
+    else:
+        break 
